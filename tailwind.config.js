324c0b2e3762d16ba7f8b1bbdfbec54e27588845
Adnan <sirgeant10@gmail.com>
2023-05-31 19:11:06 +0600

    add prettier plugin for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("prettier-plugin-tailwindcss"),
+  ],
 };
