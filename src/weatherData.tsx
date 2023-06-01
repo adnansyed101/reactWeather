e9e726811a21de6d9efbc224a9651ef80d05b04f
Adnan <sirgeant10@gmail.com>
2023-06-01 21:18:57 +0600

    Create weatherData.tsx

---
+++ src/weatherData.tsx
@@ -0,0 +1,15 @@
+const getWeatherData = async (location: string, degreetype: string) => {
+  try {
+    const key = "9ee2353a7a9004a8d0d3701c47840223";
+    const response = await fetch(`
+    https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${key}&units=${degreetype}`);
+
+    const weatherData = await response.json();
+
+    return weatherData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default getWeatherData;
