6c661ee641300b30da198dc05c2b17a5ccb262dc
Adnan <sirgeant10@gmail.com>
2023-06-03 22:24:36 +0600

    add p element

--- src/components/ShowWeather.tsx
+++ src/components/ShowWeather.tsx
@@ -14,17 +14,26 @@ const ShowWeather = ({ data }: ShowWeatherProps) => {
       return <HourlyCard key={index} hourlyCast={item} />;
     });
 
+  console.log(data);
+
   return (
-    <div className="showWeather my-4 px-4">
-      <div className=" my-4 flex flex-col items-center border-y-2 px-10">
-        <h3 className="text-2xl lg:text-3xl">{city.name}</h3>
-        <h4 className="text-xl lg:text-2xl">{list[0].weather[0].main}</h4>
-        <h1 className="my-2 text-4xl md:text-5xl">{list[0].main.temp}&deg;</h1>
-        <p className="text-sm">
-          H:{list[0].main.temp_max}&deg; || L:{list[0].main.temp_max}&deg;
-        </p>
+    <div className="my-4 px-4">
+      <div className="showWeather">
+        <div className=" my-4 flex flex-col items-center px-10">
+          <h3 className="text-2xl lg:text-3xl">{city.name}</h3>
+          <h4 className="text-xl lg:text-2xl">{list[0].weather[0].main}</h4>
+          <h1 className="my-2 text-4xl md:text-5xl">
+            {list[0].main.temp}&deg;
+          </h1>
+          <p className="text-sm">
+            H:{list[0].main.temp_max}&deg; || L:{list[0].main.temp_max}&deg;
+          </p>
+        </div>
+        <div className="flex overflow-x-scroll border-t-2 lg:ml-64">
+          {cardElement}
+        </div>
       </div>
-      <div className="flex overflow-x-scroll lg:ml-64">{cardElement}</div>
+      <div className="text-center mt-6">Today: {list[0].weather[0].description}</div>
     </div>
   );
 };
